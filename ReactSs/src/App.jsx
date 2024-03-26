360193ee2568980faa8570b1afe770019efe54c3
Askerbeksaan <askerbeksaan3@gmail.com>
2024-03-26 21:24:07 +0600

    first commit

---
+++ ReactSs/src/App.jsx
@@ -0,0 +1,15 @@
+import banner from './assets/Banner block.png'
+import Header from './Header.jsx'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <img src={banner} alt="" />
+    </>
+  )
+}
+
+export default App
---
+++ ReactSs/src/Header.jsx
@@ -0,0 +1,15 @@
+import korzina from './assets/button _ black _ icon.png'
+import logo from './assets/Group.png'
+export default function Header() {
+    return (
+        <>
+        <img src={logo} alt="" />
+        <p>QuickMeal</p>
+        <div>
+            <a href="">Home</a>
+            <a href="">All restaurants</a>
+            <a href=""><img src={korzina} alt="" /> </a>
+        </div>
+        </>
+    )
+}
